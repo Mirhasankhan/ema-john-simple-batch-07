1c0666c3b9bf8f93ab1f10a90bde936438431441
Mirhasankhan <mirhasan000034@gmail.com>
2023-03-29 21:45:30 +0600

    fake db added

--- src/Components/Shop/Shop.jsx
+++ src/Components/Shop/Shop.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import Cart from '../Cart/Cart';
 import Product from '../Product/Product';
+import { addToDb } from '../Utility/fakeDb';
 import './Shop.css';
 
 const Shop = () => {
@@ -12,10 +13,12 @@ const Shop = () => {
         .then(res => res.json())
         .then(data => setProducts(data))
     },[])
+    
 
     const handleAddToCart = (product)=>{
         const newCart = [...cart, product];
         setCart(newCart);
+        addToDb(product.id)
     }
     return (
         <div className='shop-container'>
